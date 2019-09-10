44d9bdf794a19de315dca112a5f646dfca97c986
alexandra <alexaspalato@gmail.com>
2019-09-10 09:31:05 +0200

    todo

--- src/components/StoriesGrid.js
+++ src/components/StoriesGrid.js
@@ -4,6 +4,7 @@ import { jsx, Flex } from "theme-ui"
 import Story from "./Story"
 
 const StoriesGrid = ({ results, addPick, picks }) => {
+  // TODO: make disable work after reload from localStorage
   return (
     <Flex sx={{ flexWrap: `wrap` }}>
       {results !== [] &&
