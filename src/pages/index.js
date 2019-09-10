d62598a05485354296bb261b79a04b87a2b55dc0
alexandra <alexaspalato@gmail.com>
2019-09-10 13:34:23 +0200

    remove local storage because problems on build

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ export const VoteContext = createContext()
 const IndexPage = ({ data }) => {
   const { stories, categories } = data.wpgraphql
 
+  // https://github.com/gatsbyjs/gatsby/issues/309
   const windowGlobal = typeof window !== "undefined" && window
   const ls = windowGlobal.localStorage
 
@@ -26,7 +27,8 @@ const IndexPage = ({ data }) => {
   const [results, setStories] = useState(stories.nodes)
 
   //Initial picks: taking them from localstorage
-  const [picks, setPicks] = useState(() => JSON.parse(ls.getItem("picks")))
+  // const [picks, setPicks] = useState(() => JSON.parse(ls.getItem("picks")))
+  const [picks, setPicks] = useState([])
 
   const [vote, setVote] = useState({
     clientMutationId: "submitVote",
@@ -39,7 +41,7 @@ const IndexPage = ({ data }) => {
   useEffect(() => {
     //getting the votesInput from the picks storyId
     setVote({ ...vote, votesInput: picks.map(pick => pick.storyId) })
-    ls.setItem("picks", JSON.stringify(picks))
+    // ls.setItem("picks", JSON.stringify(picks))
   }, [picks])
 
   //Filter stories grid on real time from title
