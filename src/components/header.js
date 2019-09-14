d27e9a485c2e21e40ca40d0aefe4b6a6935ab00e
alexandra <alexaspalato@gmail.com>
2019-09-14 16:36:22 +0200

    header-bgimage

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,63 @@
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 /** @jsx jsx */
-import { jsx, Styled, Flex, Box, Container } from "theme-ui"
-import headerBg from "../images/header-bg.png"
+import { jsx, Styled, Container } from "theme-ui"
 
-const Header = ({ siteTitle, description }) => (
-  <header
-    sx={{
-      p: `90px 30px 50px `,
-      background: `black url(${headerBg}) no-repeat 33% top`,
-      backgroundSize: `fit`,
-    }}
-  >
-    <Container>
-      <Styled.h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          sx={{
-            color: `white`,
-            textDecoration: `none`,
-            fontSize: [`40px`, `72px`],
-            fontWeight: 300,
-            span: {
-              color: `primary`,
-            },
-          }}
-          dangerouslySetInnerHTML={{ __html: siteTitle }}
-        />
-      </Styled.h1>
-      <box
+import BgImage from "gatsby-background-image"
+
+const Header = ({ siteTitle, description }) => {
+  const data = useStaticQuery(graphql`
+    query bgImageQuery {
+      file(relativePath: { eq: "header-bg.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <BgImage
+      fluid={data.file.childImageSharp.fluid}
+      backgroundColor="black"
+      style={{ backgroundSize: `fit`, backgroundPosition: `40% top` }}
+    >
+      <header
         sx={{
-          color: `white`,
-          fontSize: `1.6rem`,
-          fontWeight: 300,
-          lineHeight: `2.4rem`,
+          p: `90px 30px 50px `,
         }}
-        dangerouslySetInnerHTML={{ __html: description }}
-      />
-    </Container>
-  </header>
-)
+      >
+        <Container>
+          <Styled.h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              sx={{
+                color: `white`,
+                textDecoration: `none`,
+                fontSize: [`40px`, `72px`],
+                fontWeight: 300,
+                span: {
+                  color: `primary`,
+                },
+              }}
+              dangerouslySetInnerHTML={{ __html: siteTitle }}
+            />
+          </Styled.h1>
+          <box
+            sx={{
+              color: `white`,
+              fontSize: `1.6rem`,
+              fontWeight: 300,
+              lineHeight: `2.4rem`,
+            }}
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        </Container>
+      </header>
+    </BgImage>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
