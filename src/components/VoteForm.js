641ac042b478ef6bbf2c13caca5f401f3600fb8f
alexandra <alexaspalato@gmail.com>
2019-09-15 11:54:41 +0200

    useMutation

--- src/components/VoteForm.js
+++ src/components/VoteForm.js
@@ -3,6 +3,7 @@ import { jsx, Box } from "theme-ui"
 import { useContext, useState } from "react"
 import { navigate, Link, useStaticQuery, graphql } from "gatsby"
 import { Mutation } from "react-apollo"
+import { useApolloClient, useMutation } from "@apollo/react-hooks"
 import { gql } from "apollo-boost"
 import { Input, Textarea } from "@rebass/forms"
 import { VoteContext } from "../pages"
@@ -33,127 +34,117 @@ const VoteForm = ({ setVote }) => {
 
   const existingVotesMails = data.wpgraphql.votes.nodes.map(item => item.title)
 
-  console.log(existingVotesMails, vote)
+  const [voteMutation, { data }] = useMutation(SUBMIT_VOTE_MUTATION)
 
   return (
-    <Mutation mutation={SUBMIT_VOTE_MUTATION}>
-      {(voteMutation, { data, errors, loading }) => {
-        return (
-          <Box
+    <Box
+      sx={{
+        bg: `primary`,
+        p: [`60px 30px`, `60px 50px`],
+        fontSize: [2, 3],
+        fontFamily: `body`,
+      }}
+    >
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          voteMutation({
+            variables: {
+              input: vote,
+            },
+          })
+          existingVotesMails.includes(vote.emailInput)
+            ? setDisplayError("you cannot submit several votes from this email")
+            : navigate("/thank-you/")
+        }}
+      >
+        <Box
+          sx={{
+            fontFamily: `heading`,
+            color: `white`,
+            fontSize: [`40px`, `60px`],
+            textTransform: `capitalize`,
+          }}
+        >
+          Make it official!
+        </Box>
+        <Box>
+          Lock in your vote{" "}
+          <abr
             sx={{
-              bg: `primary`,
-              p: [`60px 30px`, `60px 50px`],
-              fontSize: [2, 3],
-              fontFamily: `body`,
+              color: `#ff0000`,
+              fontSize: `16px`,
+              position: `relative`,
+              top: `-5px`,
+              left: `-7px`,
             }}
           >
-            <form
-              onSubmit={e => {
-                e.preventDefault()
-                voteMutation({
-                  variables: {
-                    input: vote,
-                  },
-                })
-                existingVotesMails.includes(vote.emailInput)
-                  ? setDisplayError(
-                      "you cannot submit several votes from this email"
-                    )
-                  : navigate("/thank-you/")
-              }}
-            >
-              <Box
-                sx={{
-                  fontFamily: `heading`,
-                  color: `white`,
-                  fontSize: [`40px`, `60px`],
-                  textTransform: `capitalize`,
-                }}
-              >
-                Make it official!
-              </Box>
-              <Box>
-                Lock in your vote{" "}
-                <abr
-                  sx={{
-                    color: `#ff0000`,
-                    fontSize: `16px`,
-                    position: `relative`,
-                    top: `-5px`,
-                    left: `-7px`,
-                  }}
-                >
-                  *
-                </abr>
-              </Box>
-              <Input
-                type="email"
-                placeholder="email address"
-                value={vote.emailInput}
-                onChange={e => setVote({ ...vote, emailInput: e.target.value })}
-                required
-                sx={{ mb: `20px` }}
-              />
-              <Box>Tell us the reason for your #1 Pick:</Box>
-              <Textarea
-                placeholder="Let us know here."
-                sx={{ mb: `10px` }}
-                value={vote.messageInput}
-                onChange={e =>
-                  setVote({ ...vote, messageInput: e.target.value })
-                }
-              />
-              {displayError && (
-                <Box
-                  sx={{
-                    textAlign: `center`,
-                    color: `#bb0909`,
-                    fontSize: 1,
-                    mb: `15px`,
-                  }}
-                >
-                  {displayError}
-                </Box>
-              )}
-              <input
-                type="submit"
-                value="Vote Now"
-                sx={{ variant: `buttons.submit` }}
-              />
-            </form>
-            <Box
-              sx={{
-                color: `#bb0909`,
-                fontSize: 1,
-                textAlign: `center`,
-                fontWeight: 300,
-                mt: `10px`,
-              }}
-            >
-              *Required
-            </Box>
-            <Link
-              to="./terms-and-conditions"
-              sx={{
-                color: `#382500`,
-                fontWeight: 900,
-                textAlign: `center`,
-                textDecoration: `underline`,
-                fontSize: 1,
-                display: `block`,
-                mt: `5px`,
-                cursor: `pointer`,
-                "&:hover": {
-                  color: lighten(0.1, `#382500`),
-                },
-              }}
-            >
-              Terms and Conditions
-            </Link>
+            *
+          </abr>
+        </Box>
+        <Input
+          type="email"
+          placeholder="email address"
+          value={vote.emailInput}
+          onChange={e => setVote({ ...vote, emailInput: e.target.value })}
+          required
+          sx={{ mb: `20px` }}
+        />
+        <Box>Tell us the reason for your #1 Pick:</Box>
+        <Textarea
+          placeholder="Let us know here."
+          sx={{ mb: `10px` }}
+          value={vote.messageInput}
+          onChange={e => setVote({ ...vote, messageInput: e.target.value })}
+        />
+        {displayError && (
+          <Box
+            sx={{
+              textAlign: `center`,
+              color: `#bb0909`,
+              fontSize: 1,
+              mb: `15px`,
+            }}
+          >
+            {displayError}
           </Box>
-        )
-      }}
-    </Mutation>
+        )}
+        <input
+          type="submit"
+          value="Vote Now"
+          sx={{ variant: `buttons.submit` }}
+        />
+      </form>
+      <Box
+        sx={{
+          color: `#bb0909`,
+          fontSize: 1,
+          textAlign: `center`,
+          fontWeight: 300,
+          mt: `10px`,
+        }}
+      >
+        *Required
+      </Box>
+      <Link
+        to="./terms-and-conditions"
+        sx={{
+          color: `#382500`,
+          fontWeight: 900,
+          textAlign: `center`,
+          textDecoration: `underline`,
+          fontSize: 1,
+          display: `block`,
+          mt: `5px`,
+          cursor: `pointer`,
+          "&:hover": {
+            color: lighten(0.1, `#382500`),
+          },
+        }}
+      >
+        Terms and Conditions
+      </Link>
+    </Box>
   )
 }
 
