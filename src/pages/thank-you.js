0f9760b91150d11f8177a3e0eda3b59973929094
alexandra <alexaspalato@gmail.com>
2019-10-08 10:51:40 +0200

    fb message corrections

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -182,10 +182,12 @@ const ThankYou = props => {
           >
             <Box>
               <a
-                href={`https://www.facebook.com/sharer/sharer.php?u=https://www.gaimanfavorites.com/&quote=My favorite Neil Gaiman story is "[[${picks.length >
+                href={`https://www.facebook.com/sharer/sharer.php?u=https://www.gaimanfavorites.com/&quote=My favorite Neil Gaiman story is ${picks.length >
                   0 &&
-                  picks[0]
-                    .title}]]". What's yours? Next year, William Morrow will publish the ultimate collection of Neil Gaiman stories — a must-have for longtime fans and new readers alike. Your vote will help Neil choose the stories to include. Vote now at https://www.gaimanfavorites.com. `}
+                  picks[0].title.replace(
+                    /<[^>]*>?/gm,
+                    ""
+                  )}. What's yours? Next year, William Morrow will publish the ultimate collection of Neil Gaiman stories — a must-have for longtime fans and new readers alike. Your vote will help Neil choose the stories to include. Vote now at https://www.gaimanfavorites.com. `}
                 target="_blank"
                 rel="noopener noreferrer"
               >
